a1efd8496ce76f3a433b2df8b71c7652e1985a66
Felipe Haddad <165858976+FelipeHaddad@users.noreply.github.com>
2025-09-03 21:54:18 -0300

    Update Main.java

--- Exercicio/Main.java
+++ Exercicio/Main.java
@@ -2,6 +2,16 @@ import Arvore.*;
 
 public class Main {
     public static void main(String[] args) {
+        Arvore arvore = new Arvore();
 
+        No raiz = new No(10);
+        raiz.esq = new No(5);
+        raiz.dir = new No(20);
+        raiz.dir.esq = new No(15);
+        raiz.dir.dir = new No(30);
+
+        arvore.setRaiz(raiz);
+
+        System.out.println("Altura da árvore: " + arvore.alturaArvore());
     }
 }
