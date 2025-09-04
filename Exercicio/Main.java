24f0ed009fd27f8fe1af22eab6be1b58af5974dd
Felipe Haddad <165858976+FelipeHaddad@users.noreply.github.com>
2025-09-04 09:38:23 -0300

    Update Main.java

--- Exercicio/Main.java
+++ Exercicio/Main.java
@@ -4,14 +4,16 @@ public class Main {
     public static void main(String[] args) {
         Arvore arvore = new Arvore();
 
-        No raiz = new No(10);
-        raiz.esq = new No(5);
-        raiz.dir = new No(20);
-        raiz.dir.esq = new No(15);
-        raiz.dir.dir = new No(30);
+        No raiz = new No(10, 0);
+        raiz.esq = new No(5, 1);
+        raiz.dir = new No(20, 1);
+        raiz.dir.esq = new No(15, 2);
+        raiz.dir.dir = new No(30, 2);
 
         arvore.setRaiz(raiz);
 
         System.out.println("Altura da árvore: " + arvore.alturaArvore());
+        arvore.niveisNos();
+        arvore.mostrarHierarquico();
     }
 }
