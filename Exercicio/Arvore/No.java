df2dba026334c0dce26fb5088fe356044f60fde0
Felipe Haddad <165858976+FelipeHaddad@users.noreply.github.com>
2025-09-04 09:38:06 -0300

    Update No.java

--- Exercicio/Arvore/No.java
+++ Exercicio/Arvore/No.java
@@ -3,14 +3,25 @@ package Arvore;
 public class No {
     int chave;
     public No esq, dir;
+    int nivel;
 
-    public No(int chave) {
+    public No() {
+        this.chave = 0;
+        this.nivel = 0;
+        this.esq = null;
+        this.dir = null;
+    }
+
+    public No(int chave, int nivel) {
         this.chave = chave;
+        this.nivel = nivel;
         this.esq = null;
         this.dir = null;
     }
 
     public No getEsq() { return this.esq; }
+    public void setLeft(No node) { this.esq = node;}
     public No getDir() { return this.dir; }
+    public void setRight(No node) { this.dir = node;}
     public int getChave() { return this.chave; }
 }
